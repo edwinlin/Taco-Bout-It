a7a3e9d5b4bd1fb83b23c5ff96988e04b93f6a79
github username <github email address>
2019-10-10 14:18:16 -0400

    still working

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -2,6 +2,7 @@ const express = require('express');
 const userController = require('../controllers/UserController.js')
 
 const userRouter = express.Router()
+
 userRouter.post('/add', userController.getUser, userController.createUser, (req, res)=>{
   // console.log("REQ", req.body)
 })
